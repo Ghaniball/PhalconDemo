df8cf5870f9cbf55e03dbe487922886b6267da1c
igodorogea <igodorogea@winify.com>
2014-05-07 13:59:53 +0300

    Refactor mail and log jobs

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+$mailConfig = require APP_DIR . '/config/mail.php';
+
 return new \Phalcon\Config(array(
 	/* 	'database' => array(
 	  'adapter'     => 'Mysql',
@@ -10,30 +12,19 @@ return new \Phalcon\Config(array(
 	  ), */
 	'logPath' => __DIR__ . '/../../logs/',
 	'application' => array(
-		'controllersDir' => __DIR__ . '/../../app/controllers/',
-		'modelsDir' => __DIR__ . '/../../app/models/',
-		'viewsDir' => __DIR__ . '/../../app/views/',
-		'pluginsDir' => __DIR__ . '/../../app/plugins/',
-		'formsDir' => __DIR__ . '/../../app/forms/',
-		'libraryDir' => __DIR__ . '/../../app/library/',
-		'cacheDir' => __DIR__ . '/../../app/cache/',
+		'controllersDir' => APP_DIR . '/controllers/',
+		'modelsDir' => APP_DIR . '/models/',
+		'viewsDir' => APP_DIR . '/views/',
+		'pluginsDir' => APP_DIR . '/plugins/',
+		'formsDir' => APP_DIR . '/forms/',
+		'libraryDir' => APP_DIR . '/library/',
+		'cacheDir' => APP_DIR . '/cache/',
 		'baseUri' => '/',
 	),
-	'mail' => array(
-		'fromName' => 'Phalcon Term',
-		'fromEmail' => '',
-		'smtp' => array(
-			'server' => 'smtp.gmail.com',
-			'port' => 465,
-			'security' => 'ssl',
-			'username' => '',
-			'password' => '',
-		)
-	),
+	'mail' => $mailConfig,
 	'messages' => array(
 		'requestFails' => 'You came to the right address! We see you have the potential of improving your position in the Google search for the "%s" keyword you specified!',
 		'foundInResults' => 'Domain mentioned is Detected in the first 8 positions of the "%s" request based on local search (Google.de) and German Language We see that you already did a good job positioning yourself for the Keyword specified in the first 8 positions of the Google.de search and we will be glad to help you improve your position for other important Keywords or improve your actual positions locally and internationally!',
-		'notFoundInResults' => 'Domain mentioned is NOT detected in the first 8 positions of the "%s" request based on local search (Google.de) and German Language You came to the right address! Being out of first 8 positions in the Google search for the important Keyword you specified we can definitely help you here!',
-		
+		'notFoundInResults' => 'Domain mentioned is NOT detected in the first 8 positions of the "%s" request based on local search (Google.de) and German Language You came to the right address! Being out of first 8 positions in the Google search for the important Keyword you specified we can definitely help you here!'
 	)
 ));
--- app/forms/SearchForm.php
+++ app/forms/SearchForm.php
@@ -5,7 +5,6 @@
  *
  * @author igodorogea
  */
-
 use Phalcon\Forms\Form;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Element\Submit;
@@ -18,40 +17,42 @@ use Phalcon\Validation\Validator\Regex as RegexValidator;
 class SearchForm extends Form {
 
 	public function initialize($entity = null, $options = null) {
-		$keyword = new Text('keyword');
+		$keyword = new Text('keyword', array(
+			'placeholder' => "Keyword",
+		));
 
 		//$keyword->setLabel('Keyword');
 
 		$keyword->addValidators(array(
-            new PresenceOf(array(
-                'message' => 'The keyword is required'
-            )),
-            new StringLength(array(
-                'min' => 3,
-                'messageMinimum' => 'Keyword is too short. Minimum 3 characters'
-            )),
+			new PresenceOf(array(
+				'message' => 'The keyword is required'
+					)),
+			new StringLength(array(
+				'min' => 3,
+				'messageMinimum' => 'Keyword is too short. Minimum 3 characters'
+					)),
 		));
 
 		$this->add($keyword);
-		
+
 		$doamin = new Text('domain', array(
-                    'placeholder' => "Domain",
-                ));
+			'placeholder' => "Domain",
+		));
 
 		//$doamin->setLabel('Domain');
 
 		$doamin->addValidators(array(
-                    new PresenceOf(array(
-                        'message' => 'The domain is required'
-                    )),
-                    new RegexValidator(array(
-                                        'pattern' => '/.*/',
-                                        'message' => 'The domain name is invalid'
-                                )),
+			new PresenceOf(array(
+				'message' => 'The domain is required'
+					)),
+			new RegexValidator(array(
+				'pattern' => '/.*/',
+				'message' => 'The domain name is invalid'
+					)),
 		));
 
 		$this->add($doamin);
-		
+
 		$di = Phalcon\DI::getDefault();
 		$security = $di['security'];
 		// CSRF
@@ -64,23 +65,23 @@ class SearchForm extends Form {
 
 		$this->add($csrf);
 
-		// Sign Up
-                $submit = new Submit('Check', array(
-                    "value" => "Analyse Starten",
-                ));
-                
+		// Submit
+		$submit = new Submit('Check', array(
+			"value" => "Analyse Starten",
+		));
+
 		$this->add($submit);
 	}
 
-    /**
-     * Prints messages for a specific element
-     */
-    public function messages($name)
-    {
-        if ($this->hasMessagesFor($name)) {
-            foreach ($this->getMessagesFor($name) as $message) {
-                $this->flash->error($message);
-            }
-        }
-    }
+	/**
+	 * Prints messages for a specific element
+	 */
+	public function messages($name) {
+		if ($this->hasMessagesFor($name)) {
+			foreach ($this->getMessagesFor($name) as $message) {
+				$this->flash->error($message);
+			}
+		}
+	}
+
 }
